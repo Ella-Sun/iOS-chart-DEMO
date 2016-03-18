04f6b4d763acd65e57d70a53e97aed397471af42
sunhong <sunhong@ucfgroup.com>
2016-03-18 16:29:53 +0800

    Initial Commit

---
+++ BarChartSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BarChartSample
+//
+//  Created by IOS-Sun on 16/3/18.
+//  Copyright © 2016年 IOS-Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BarChartSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarChartSample
+//
+//  Created by IOS-Sun on 16/3/18.
+//  Copyright © 2016年 IOS-Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
